8dcfcb94d945420d114798cedad0f8cc96241a25
Carl <carloliver365@gmail.com>
2026-09-23 14:05:15 +0300

    Add files via upload

---
+++ Units.c
@@ -0,0 +1,27 @@
+// Single Line Comment ( Program to display Hello World )
+
+// Multiline Comment
+
+/*
+Author: Carl Oliver
+Reg Number: BCS-05-0067/2026
+Description: Hello World Program
+Date: 
+Version: 1
+*/
+
+// Pre-processor directive( Anything starting with #)
+
+#include <stdio.h> // scanf(), printf()
+
+
+// Main function
+int main(){
+	// display 
+	printf("Units Offered this Semester: \n");
+	printf("Data Structures. \n");
+	printf("Computer Networks. \n ");
+    printf("Database Systems. \n");
+    printf("Software Engineering. \n \n");	
+	return 0; // execution successfull.
+}
